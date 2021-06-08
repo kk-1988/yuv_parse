843d03beee4cd4ff1d10004718e68c796102a869
xiaobo.kong <xiaobo.kong@aliyun.com>
2021-06-08 10:18:39 +0800

    rgb24 转 yuv420格式的demo

---
+++ rgb_to_yuv420.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+unsigned char clip_value(unsigned char x, unsigned char min_val,unsigned char max_val)
+{   
+    if(x > max_val)
+        return max_val;
+    else if(x < min_val)
+        return min_val;
+    else
+        return x;
+}
+
+//RGB TO YUV420
+bool RGB24_TO_YUV420(unsigned char *RgbBuf, int w, int h, unsigned char *yuvBuf)
+{
+    unsigned char *ptrY, *ptrU, *ptrV, *ptrRGB;
+    memset(yuvBuf,0,w*h*3/2);
+    ptrY = yuvBuf;
+    ptrU = yuvBuf + w * h;
+    ptrV = ptrU + (w*h*1/4);
+    unsigned char y, u, v, r, g, b;
+    for(int j = 0; j < h; j++)
+    {
+        ptrRGB = RgbBuf + w*j*3;
+        for(int i = 0;i < w;i++)
+        {
+            r = *(ptrRGB++);
+            g = *(ptrRGB++);
+            b = *(ptrRGB++);     
+            y = (unsigned char)( ( 66 * r + 129 * g +  25 * b + 128) >> 8) + 16;
+            u = (unsigned char)( ( -38 * r -  74 * g + 112 * b + 128) >> 8) + 128;
+            v = (unsigned char)( ( 112 * r -  94 * g -  18 * b + 128) >> 8) + 128;
+            
+            
+        }
+    }
+}
+
+int main(int argc,char *argv[])
+{
+
+    printf("rgb24 convert to yuv420...\r\n");
+    return 0;
+}
